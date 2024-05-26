939b3b81ee3835e4d90afd6df4f2c99467e2f6a8
Davi1ss <143420728+Davi1ss@users.noreply.github.com>
2024-05-25 20:01:02 -0700

    Create Vehicle.java

---
+++ Unidad_3.3/Vehicle.java
@@ -0,0 +1,23 @@
+
+// Clase abstracta Vehicle
+abstract class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void start() {
+        System.out.println(name + " is starting.");
+    }
+
+    public void stop() {
+        System.out.println(name + " is stopping.");
+    }
+
+    public abstract void accelerate();
+}
