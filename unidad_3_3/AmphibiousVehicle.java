cf5e85942fe1afe78819111aad723b4f6ccf566c
Davi1ss <143420728+Davi1ss@users.noreply.github.com>
2024-05-26 20:02:09 -0700

    Update AmphibiousVehicle.java

--- unidad_3_3/AmphibiousVehicle.java
+++ unidad_3_3/AmphibiousVehicle.java
@@ -2,23 +2,31 @@ package unidad_3_3;
 
 // Clase AmphibiousVehicle
 class AmphibiousVehicle extends Vehicle implements LandVehicle, WaterVehicle {
+    private int landSpeed;
+    private int waterSpeed;
 
     public AmphibiousVehicle(String name) {
         super(name);
+        this.landSpeed = 0;
+        this.waterSpeed = 0;
     }
 
     @Override
-    public void accelerate() {
-        System.out.println(getName() + " is accelerating.");
+    public void accelerate(int increment) {
+        landSpeed += increment;
+        waterSpeed += increment / 2; // Supongamos que la velocidad del agua aumenta a la mitad de la velocidad terrestre
+        System.out.println(getName() + " Esta acelerando. Land speed: " + landSpeed + " km/h, Water speed: " + waterSpeed + " km/h.");
     }
 
     @Override
     public void driveOnLand() {
-        System.out.println(getName() + " is driving on land.");
+        landSpeed = 20; // Arbitrary initial land speed
+        System.out.println(getName() + " está conduciendo por tierra a " + landSpeed + " km/h.");
     }
 
     @Override
     public void sailOnWater() {
-        System.out.println(getName() + " is sailing on water.");
+        waterSpeed = 10; // Arbitrary initial water speed
+        System.out.println(getName() + " Esta navegando por agua a " + waterSpeed + " km/h.");
     }
 }
