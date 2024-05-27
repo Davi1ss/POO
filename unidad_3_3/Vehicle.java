08809dbb735f8b1536dcf96b9692830ef4de6a99
Davi1ss <143420728+Davi1ss@users.noreply.github.com>
2024-05-26 20:04:11 -0700

    Update Vehicle.java

--- unidad_3_3/Vehicle.java
+++ unidad_3_3/Vehicle.java
@@ -4,22 +4,29 @@ package unidad_3_3;
 // Clase abstracta Vehicle
 abstract class Vehicle {
     private String name;
+    private int speed;
 
     public Vehicle(String name) {
         this.name = name;
+        this.speed = 0;
     }
 
     public String getName() {
         return name;
     }
 
+    public int getSpeed() {
+        return speed;
+    }
+
     public void start() {
-        System.out.println(name + " is starting.");
+        System.out.println(name + " está empezando.");
     }
 
     public void stop() {
-        System.out.println(name + " is stopping.");
+        speed = 0;
+        System.out.println(name + " está deteniendose.");
     }
 
-    public abstract void accelerate();
+    public abstract void accelerate(int increment);
 }
